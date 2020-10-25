9f449a327f3df84b7323a1ca0564e6cf2ec1638c
Martin Mlostek <mlostek@gmail.com>
2020-10-25 15:25:51 +0100

    added debug logging

--- Cloudy/WebViewControllerBridge.swift
+++ Cloudy/WebViewControllerBridge.swift
@@ -4,14 +4,26 @@ import Foundation
 import WebKit
 import GameController
 
+
 /// Main module that connects the web views controller scripts to the native controller handling
 class WebViewControllerBridge: NSObject, WKScriptMessageHandlerWithReply {
+
+    /// Remember last pressed keys to avoid log spamming
+    private var lastPressedKeys: String? = nil
 
     /// Handle user content controller with proper native controller data reply
     func userContentController(_ userContentController: WKUserContentController,
                                didReceive message: WKScriptMessage,
                                replyHandler: @escaping (Any?, String?) -> Void) {
-//        print(GCController.controllers().first?.extendedGamepad?.toJson())
+        #if DEBUG
+            // debug log
+            if let pressedKeys = GCController.controllers().first?.extendedGamepad?.pressedKeys {
+                if lastPressedKeys != pressedKeys {
+                    print(pressedKeys)
+                }
+                lastPressedKeys = pressedKeys
+            }
+        #endif
         replyHandler(GCController.controllers().first?.extendedGamepad?.toJson(), nil)
     }
 }
